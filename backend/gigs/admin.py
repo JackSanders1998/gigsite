a1fcb36d2ad56712ffd235a4120380902cca3bde
JackSanders1998 <jacklewissanders@gmail.com>
2021-09-08 15:37:43 -0500

    set up admin to add concerts

--- backend/gigs/admin.py
+++ backend/gigs/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Todo, Question, Gig
+
+
+class TodoAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'completed')
+
+class GigAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'start_datetime', 'end_datetime')
 
 # Register your models here.
+
+admin.site.register(Gig, GigAdmin)
