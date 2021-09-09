dd990311e71c1a1726463d0b2f5bd5abf98cf728
JackSanders1998 <jacklewissanders@gmail.com>
2021-09-09 13:59:06 -0500

    bootstrapped a login together

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -6,8 +6,6 @@ If it's going to be an object, default this to an empty object.
 **/
 export const apiData = writable([]);
 
-/** Data transformation.
-**/
 export const gigDetails = derived(apiData, ($apiData) => {
     if ($apiData){
         console.log("in apiData");
@@ -16,3 +14,21 @@ export const gigDetails = derived(apiData, ($apiData) => {
     console.log("not in apiData");
     return [];
   });
+
+
+export const isAuthenticated = writable(false);
+export const user = writable({});
+export const popupOpen = writable(false);
+export const error = writable();
+
+export const gigs = writable([]);
+
+export const user_gigs = derived([gigs, user], ([$gigs, $user]) => {
+  let logged_in_user_gigs = [];
+
+  if ($user && $user.email) {
+    logged_in_user_gigs = $gigs.filter((gig) => gig.user === $user.email);
+  }
+
+  return logged_in_user_gigs;
+});
