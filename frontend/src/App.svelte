fb5493b3a446e85ddbfb5559aa75a7898a25eab0
JackSanders1998 <jacklewissanders@gmail.com>
2021-09-08 22:47:35 -0500

    test API call

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,10 +1,38 @@
 <script>
 	export let name;
+
+	import { onMount } from "svelte";
+
+	const apiURL = "http://127.0.0.1:8000/api/gigs/?format=json";
+	let data = [];
+	let data_test = "testing data";
+	onMount(async function() {
+        const response = await fetch(apiURL, {
+			method: "put",
+    credentials: "same-origin",
+    body: JSON.stringify(data)
+		});
+		console.log("response", response);
+        data = await response.json();
+		console.log('data', data);
+    });
+
 </script>
 
 <main>
 	<h1>Hello {name}!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+
+	<div>
+		{data_test}
+		{#each data_test as item }
+			<div>
+				<p> "test" </p>
+				<p> {item.title} </p>
+			</div>
+		{/each}
+	</div>
+
 </main>
 
 <style>
